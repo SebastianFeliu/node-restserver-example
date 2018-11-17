049efd8bf0c9795da58a1cf9398988ec9ac1212e
SebastianFeliu <sebastianfeliu97@gmail.com>
2018-11-17 01:47:21 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -19,7 +19,7 @@ let verificarToken = (req,res,next) => {
 
         req.usuario = decoded.usuario;
         next();
-    })
+    });
 
     console.log(token)
 };
@@ -44,7 +44,30 @@ let verificarAdmin_Role = (req,res,next) => {
 
 }
 
+// ===============================
+// Verificar TOKEN para imagen
+// ===============================
+
+let verificarTokenImg = (req,res,next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
 module.exports = {
     verificarToken,
-    verificarAdmin_Role
+    verificarAdmin_Role,
+    verificarTokenImg
+
 }
